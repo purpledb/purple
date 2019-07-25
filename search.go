2c5d69ed22c782e8940821395ca27cdf8981f303
lucperkins <lucperkins@gmail.com>
2019-07-24 22:01:46 -0700

    Add Search interface with Index and Query methods

---
+++ search.go
@@ -0,0 +1,51 @@
+package strato
+
+import "strato/proto"
+
+type (
+	Search interface {
+		Index(doc *Document)
+		Query(q string) []*Document
+	}
+
+	Document struct {
+		ID      string
+		Content string
+	}
+)
+
+func (d *Document) toProto() *proto.Document {
+	return &proto.Document{
+		Id:      d.ID,
+		Content: d.Content,
+	}
+}
+
+func docFromProto(docP *proto.Document) *Document {
+	return &Document{
+		ID:      docP.Id,
+		Content: docP.Content,
+	}
+}
+
+func docsFromProto(docsP []*proto.Document) []*Document {
+	docs := make([]*Document, 0)
+
+	for _, d := range docsP {
+		docs = append(docs, docFromProto(d))
+	}
+
+	return docs
+}
+
+func docsToResults(docs []*Document) *proto.SearchResults {
+	docsP := make([]*proto.Document, 0)
+
+	for _, d := range docs {
+		docsP = append(docsP, d.toProto())
+	}
+
+	return &proto.SearchResults{
+		Documents: docsP,
+	}
+}
