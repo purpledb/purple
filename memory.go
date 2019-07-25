cfc848a297dfc180233ee4beb35826198c27e0dc
lucperkins <lucperkins@gmail.com>
2019-07-24 22:02:37 -0700

    Implement Search interface in Memory backend

--- memory.go
+++ memory.go
@@ -1,16 +1,25 @@
 package strato
 
+import "golang.org/x/tools/go/ssa/interp/testdata/src/strings"
+
 type Memory struct {
 	values map[Location]*Value
+	docs   []*Document
 }
 
-var _ KV = (*Memory)(nil)
+var (
+	_ KV     = (*Memory)(nil)
+	_ Search = (*Memory)(nil)
+)
 
 func New() *Memory {
 	values := make(map[Location]*Value)
 
+	docs := make([]*Document, 0)
+
 	return &Memory{
 		values: values,
+		docs:   docs,
 	}
 }
 
@@ -32,4 +41,24 @@ func (m *Memory) Delete(location *Location) {
 	if _, ok := m.values[*location]; ok {
 		delete(m.values, *location)
 	}
+}
+
+func (m *Memory) Index(doc *Document) {
+	m.docs = append(m.docs, doc)
+}
+
+func (m *Memory) Query(q string) []*Document {
+	docs := make([]*Document, 0)
+
+	if len(m.docs) == 0 {
+		return nil
+	}
+
+	for _, doc := range m.docs {
+		if strings.Contains(doc.Content, q) {
+			docs = append(docs, doc)
+		}
+	}
+
+	return docs
 }
