21c38c6dbc39a85f0140ecaf0b96a23c9defa3cc
lucperkins <lucperkins@gmail.com>
2019-07-31 11:02:47 -0700

    Add gRPC KV cache test

--- grpc_server_test.go
+++ grpc_server_test.go
@@ -1,6 +1,8 @@
 package strato
 
 import (
+	"context"
+	"github.com/lucperkins/strato/proto"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -8,11 +10,36 @@ import (
 func TestGrpcServer(t *testing.T) {
 	is := assert.New(t)
 
+	ctx := context.Background()
+
 	srv, err := NewGrpcServer(goodServerCfg)
 
 	t.Run("Instantiation", func(_ *testing.T) {
 		is.NoError(err)
 		is.NotNil(srv)
+	})
+
+	t.Run("Cache", func(_ *testing.T) {
+		setReq := &proto.CacheSetRequest{
+			Key: "key",
+			Item: &proto.CacheItem{
+				Value: "value",
+				Ttl: 2,
+			},
+		}
+
+		empty, err := srv.CacheSet(ctx, setReq)
+		is.NoError(err)
+		is.NotNil(empty)
+
+		getReq := &proto.CacheGetRequest{
+			Key: "key",
+		}
+
+		res, err := srv.CacheGet(ctx, getReq)
+		is.NoError(err)
+		is.NotNil(res)
+		is.Equal(res.Value, "value")
 	})
 
 	is.NoError(srv.mem.Close())
