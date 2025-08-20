3fc51cfe0a014377536e508ff0e13d153240fc20
Luc Perkins <lucperkins@gmail.com>
2025-08-20 22:32:09 +0100

    Update Nix and Go stuff

--- flake.nix
+++ flake.nix
@@ -1,8 +1,32 @@
 {
   description = "Purple DB";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    {
+      self,
+      ...
+    }@inputs:
     let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
+
       # Constants
       org = "purpledb";
       version = "0.1.6";
@@ -13,101 +37,99 @@
       };
 
       # The same across all packages
-      vendorSha256 = "sha256-rRHPYuWOPIHE60vVxZMH+TwFNGlQf5fPmoqV+g0cUZg=";
-
-      # Overlays
-      overlays = [
-        (self: super: rec {
-          go = super.go_1_18;
-
-          buildGoModuleLinux = super.buildGoModule.override {
-            go = super.go // {
-              CGO_ENABLED = 0;
-              GOOS = target.os;
-              GOARCH = target.arch;
-            };
-          };
-        })
-      ];
+      vendorHash = "sha256-rRHPYuWOPIHE60vVxZMH+TwFNGlQf5fPmoqV+g0cUZg=";
     in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit overlays system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
               cmake
               go
               gotools
               protobuf
               docker
             ];
-        };
+          };
+        }
+      );
 
-        apps = {
-          default = flake-utils.lib.mkApp {
-            drv = pkgs.writeScriptBin "version" ''
-              echo ${version}
-            '';
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          http = pkgs.buildGo "purple-http";
+          grpc = pkgs.buildGo "purple-grpc";
+        }
+      );
+
+      dockerImages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          httpDocker = pkgs.buildDocker {
+            name = "purple-http";
+            port = 8080;
+            tag = "v${version}";
+          };
+
+          httpDockerLatest = pkgs.buildDocker {
+            name = "purple-http";
+            port = 8080;
+          };
+
+          grpcDocker = pkgs.buildDocker {
+            name = "purple-grpc";
+            port = 8081;
+            tag = "v${version}";
+          };
+
+          grpcDockerLatest = pkgs.buildDocker {
+            name = "purple-grpc";
+            port = 8081;
+          };
+        }
+      );
+
+      overlays.default = final: prev: rec {
+        buildGoModuleLinux = prev.buildGoModule.override {
+          go = prev.go // {
+            CGO_ENABLED = 0;
+            GOOS = target.os;
+            GOARCH = target.arch;
           };
         };
 
-        packages =
+        buildGo =
+          name:
+          final.buildGoModule {
+            inherit name vendorHash;
+            src = ./.;
+            subPackages = [ "cmd/${name}" ];
+          };
+
+        buildDocker =
+          {
+            name,
+            port,
+            tag ? "latest",
+          }:
           let
-            buildGo = name: pkgs.buildGoModule {
-              inherit name vendorSha256;
+            pkg = final.buildGoModuleLinux {
+              inherit name vendorHash;
               src = ./.;
               subPackages = [ "cmd/${name}" ];
+              maxLayers = 120;
             };
-
-            buildDocker = { name, port, tag ? "latest" }:
-              let
-                pkg = pkgs.buildGoModuleLinux {
-                  inherit name vendorSha256;
-                  src = ./.;
-                  subPackages = [ "cmd/${name}" ];
-                  maxLayers = 120;
-                };
-              in
-              pkgs.dockerTools.buildLayeredImage {
-                name = "${org}/${name}";
-                inherit tag;
-
-                config = {
-                  Entrypoint = [ "${pkg}/bin/${target.os}_${target.arch}/${name}" ];
-                  ExposedPorts."${builtins.toString port}/tcp" = { };
-                };
-              };
           in
-          rec
-          {
+          final.dockerTools.buildLayeredImage {
+            name = "${org}/${name}";
+            inherit tag;
 
-            http = buildGo "purple-http";
-            grpc = buildGo "purple-grpc";
-
-            httpDocker = buildDocker rec {
-              name = "purple-http";
-              port = 8080;
-              tag = "v${version}";
-            };
-
-            httpDockerLatest = buildDocker rec {
-              name = "purple-http";
-              port = 8080;
-            };
-
-            grpcDocker = buildDocker rec {
-              name = "purple-grpc";
-              port = 8081;
-              tag = "v${version}";
-            };
-
-            grpcDockerLatest = buildDocker rec {
-              name = "purple-grpc";
-              port = 8081;
+            config = {
+              Entrypoint = [ "${pkg}/bin/${target.os}_${target.arch}/${name}" ];
+              ExposedPorts."${builtins.toString port}/tcp" = { };
             };
           };
-      });
+      };
+    };
 }
